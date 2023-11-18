4267cc841cdfd32f347063842313119991a02f86
AtanuPanja <atanupanja9463@gmail.com>
2023-11-18 19:26:02 +0530

    Added customer-type logos, and updated the customer section with figure tags

--- my-app/components/CustomerSection.js
+++ my-app/components/CustomerSection.js
@@ -1,28 +1,26 @@
 import Image from "next/image";
 export default function CustomerSection() {
   return (
-    <div>
-      <section className="my-10 body-font">
+      <section className="bg-gray-100 py-10 body-font">
         <div className="container mx-auto">
           <h2 className="title-font text-2xl my-2 font-medium text-center sm:text-3xl">
             Our Customers
           </h2>
-          <div className="bg-gray-200 flex justify-around px-5 py-5 flex-wrap rounded-lg">
-            <div className="m-10">
-              <Image src="./logo.svg" width="100" height="100" />
-              <p className="text-center">Brand</p>
-            </div>
-            <div className="m-10">
-              <Image src="./logo.svg" width="100" height="100" />
-              <p className="text-center">Brand</p>
-            </div>
-            <div className="m-10">
-              <Image src="./logo.svg" width="100" height="100" />
-              <p className="text-center">Brand</p>
-            </div>
+          <div className=" flex justify-around px-5 pt-5 flex-wrap rounded-lg">
+            <figure className="m-10">
+              <Image src="/shopping-bag.png" width="100" height="100" />
+              <figcaption className="text-center">Shopper</figcaption>
+            </figure>
+            <figure className="m-10">
+              <Image src="/online-store.png" width="100" height="100" />
+              <figcaption className="text-center">EStore</figcaption>
+            </figure>
+            <figure className="m-10">
+              <Image src="/mall.png" width="100" height="100" />
+              <figcaption className="text-center">GenZ mall</figcaption>
+            </figure>
           </div>
         </div>
       </section>
-    </div>
   );
 }
