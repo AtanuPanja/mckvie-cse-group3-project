279caddc90201f9ab2eadb606ae929e461cfdb31
Arindam Sal <arindam.sal.edu@gmail.com>
2023-11-21 18:36:25 +0530

    Contact section removed from FAQs and Ask Doubts button added redirecting to Contact page.

--- my-app/components/About.js
+++ my-app/components/About.js
@@ -981,76 +981,17 @@ const About = () => {
                   </div>
                 </div>
               </div>
-              <section className="text-gray-600 body-font relative">
-                <div className="container px-5 py-24 mx-auto">
-                  <div className="flex flex-col text-center w-full mb-12">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              <section class="text-gray-600 body-font">
+                <div class="container px-5 pb-24 mx-auto">
+                  <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+                    <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
                       Curious Minds Welcome: Have a Question Beyond the FAQs?
                     </h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                      Let us know, and we'll get back to you with the answers
-                      you seek! Feel free to drop us a message or reach out
-                      through our contact form below.
-                    </p>
-                  </div>
-                  <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                    <div className="flex flex-wrap -m-2">
-                      <div className="p-2 w-1/2">
-                        <div className="relative">
-                          <label
-                            htmlFor="name"
-                            className="leading-7 text-sm text-gray-600"
-                          >
-                            Name
-                          </label>
-                          <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                          />
-                        </div>
-                      </div>
-                      <div className="p-2 w-1/2">
-                        <div className="relative">
-                          <label
-                            htmlFor="email"
-                            className="leading-7 text-sm text-gray-600"
-                          >
-                            Email
-                          </label>
-                          <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                          />
-                        </div>
-                      </div>
-                      <div className="p-2 w-full">
-                        <div className="relative">
-                          <label
-                            htmlFor="message"
-                            className="leading-7 text-sm text-gray-600"
-                          >
-                            My Query
-                          </label>
-                          <textarea
-                            id="message"
-                            name="message"
-                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                          ></textarea>
-                        </div>
-                      </div>
-                      <div className="p-2 w-full">
-                        <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                          Submit
-                        </button>
-                      </div>
-                      <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                        <span className="inline-flex"></span>
-                      </div>
-                    </div>
+                    <Link href="/contact" className="mr-5 ">
+                      <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">
+                        Ask Doubts
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </section>
@@ -1113,7 +1054,7 @@ const About = () => {
       </div>
       <section className="text-gray-600 body-font">
         <h1 className="heading">Transforming Supply Chains in Four Steps</h1>
-        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="container px-5 pb-24 mx-auto flex flex-wrap">
           <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
             <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
               <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
